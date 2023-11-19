0dac078b2b950f6466dcea14629a15495ce7a4f2
sy0201 <noey0201@gmail.com>
2023-11-19 20:15:23 +0900

    - add: UIView Gradation Extension 추가

--- FormSample/Extension/UIView.swift
+++ FormSample/Extension/UIView.swift
@@ -12,4 +12,26 @@ extension UIView {
         views.forEach { self.addSubview($0) }
     }
     
+    func gradationView(_ colors: [UIColor], rect: CGRect? = nil, _ isLeft: Bool = false) {
+        let name = "gradientButtonBackground"
+
+        for item in self.layer.sublayers ?? [] where item.name == name {
+            item.removeFromSuperlayer()
+        }
+
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.name = name
+        gradientLayer.frame = bounds
+        gradientLayer.colors = colors.map { $0.cgColor }
+
+        if isLeft {
+            gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+            gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+        } else {
+            gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+            gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+        }
+
+        layer.addSublayer(gradientLayer)
+    }
 }
