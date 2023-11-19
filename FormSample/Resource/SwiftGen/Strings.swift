d8df2a8428549043d46e67e1b8aa379500897ba7
sy0201 <noey0201@gmail.com>
2023-11-19 22:11:43 +0900

    - add: localizable ko 추가

--- FormSample/Resource/SwiftGen/Strings.swift
+++ FormSample/Resource/SwiftGen/Strings.swift
@@ -10,6 +10,8 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum L10n {
+  /// 환영합니다!입주에 한 발 다가가기 위한 사전확인 신청을 시작해봅시다!
+  internal static let mainMessage1 = L10n.tr("Localizable", "Main_message1", fallback: "환영합니다!입주에 한 발 다가가기 위한 사전확인 신청을 시작해봅시다!")
   /// Localizable.strings
   ///   FormSample
   /// 
@@ -17,6 +19,8 @@ internal enum L10n {
   internal static let preConfirmation = L10n.tr("Localizable", "Pre_confirmation", fallback: "사전확인")
   /// 사전확인신청
   internal static let requestPreConfirmation = L10n.tr("Localizable", "Request_Pre_confirmation", fallback: "사전확인신청")
+  /// 단지선택
+  internal static let selectApartmentComplex = L10n.tr("Localizable", "Select_Apartment_Complex", fallback: "단지선택")
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
