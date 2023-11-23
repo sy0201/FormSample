46445e721d956552b5cd2e43f33d0b99a3e2b0fe
sy0201 <noey0201@gmail.com>
2023-11-23 11:03:01 +0900

    - add: 작성하기 BottomSheet 추가

---
+++ FormSample/Screen/Controller/CreateFormBottomViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CreateFormBottomViewController.swift
+//  FormSample
+//
+//  Created by siyeon park on 2023/11/23.
+//
+
+import UIKit
+
+class CreateFormBottomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
