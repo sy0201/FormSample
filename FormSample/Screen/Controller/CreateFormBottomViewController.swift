0ed1ac2214d2cf47976e261730f13d24750299a3
sy0201 <noey0201@gmail.com>
2023-11-24 17:45:16 +0900

    - update: textView 로직 수정

--- FormSample/Screen/Controller/CreateFormBottomViewController.swift
+++ FormSample/Screen/Controller/CreateFormBottomViewController.swift
@@ -17,45 +17,20 @@ final class CreateFormBottomViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTextView()
+        setupTargetButton()
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.createFormBaseView.defectiveTextView.resignFirstResponder()
     }
-
-    func setupTextView() {
-        createFormBaseView.defectiveTextView.delegate = self
-
-    }
 }
 
-extension CreateFormBottomViewController: UITextViewDelegate {
-    func textViewDidBeginEditing(_ textView: UITextView) {
-        if createFormBaseView.defectiveTextView == Asset.Color.gray9DA4AA.color {
-            createFormBaseView.defectiveTextView.text = nil
-            createFormBaseView.defectiveTextView.textColor = Asset.Color.black.color
-        }
+extension CreateFormBottomViewController {
+    func setupTargetButton() {
+        createFormBaseView.unSelectedButton.addTarget(self, action: #selector(hiddenPhotoView), for: .touchUpInside)
     }
 
-    func textViewDidEndEditing(_ textView: UITextView) {
-//        if createFormBaseView.defectiveTextView.text.isEmpty {
-//            createFormBaseView.defectiveTextView.font = FontFamily.NotoSansKR.regular.font(size: 14)
-//            createFormBaseView.defectiveTextView.textColor = Asset.Color.gray9DA4AA.color
-//            createFormBaseView.defectiveTextView.text = L10n.formMessage16
-//        }
-
-        if createFormBaseView.defectiveTextView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-            createFormBaseView.defectiveTextView.text =  L10n.formMessage16
-            createFormBaseView.defectiveTextView.textColor = Asset.Color.gray9DA4AA.color
-        }
-    }
-
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        let inputString = text.trimmingCharacters(in: .whitespacesAndNewlines)
-        guard let oldString = textView.text, let newRange = Range(range, in: oldString) else { return true }
-        let newString = oldString.replacingCharacters(in: newRange, with: inputString).trimmingCharacters(in: .whitespacesAndNewlines)
-
-        return true
+    @objc func hiddenPhotoView() {
+        print("선택안함")
     }
 }
